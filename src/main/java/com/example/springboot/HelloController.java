96168f794839cd34c0e92564abd24c6cad06272a
falberto <falberto@vmware.com>
2022-02-11 09:57:35 +0100

    back top default

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! weeee";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
